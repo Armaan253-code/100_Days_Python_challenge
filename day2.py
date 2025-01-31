6d7d81edb9d3b76e54b0423e9beef883b037ed26
Armaan253-code <am5432107@gmail.com>
2025-01-31 11:32:26 +0530

    Add files via upload

---
+++ day2.py
@@ -0,0 +1,57 @@
+print(len("hello"))
+#Subscripting
+print("Hello"[0])
+print("Hello"[-1])
+
+#String
+print("123"+"786")
+
+#Integer= Whole number
+print(123+786)
+#Large Integer
+
+print(123_786_143)
+
+#Float= Floating point number
+
+print(3.14)
+#Bolean
+print('true')
+print('false')
+
+#len(123)->type error
+print(type("Hello"))
+print(type(786))
+print(type(3.14))
+print(type('true'))
+print(int("123")+int("786"))
+#Type conversion
+# int()
+# float()
+# str()
+# bool()
+
+#task
+name_of_the_user=input("Enter your name")
+length_of_name=len(name_of_the_user)
+print(type("Number of letters in your name: ")) #str
+print(type(length_of_name)) #int
+
+print("Number of letters in your name"+ str(length_of_name))
+
+#Mathematical Operations
+print("My age"+ str(21))
+print(123+456)
+print(3+2)
+print(3*2)
+print(5/3)
+print(5//3)
+print(2**3)
+
+#PEMDASLR
+# ()
+# **
+#* or /
+# + or -
+print(3*3+3/3-3)
+print(3*(3+3)/3-3)
