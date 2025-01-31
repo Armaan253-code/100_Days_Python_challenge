ba014e26791501061b07365239bae8cbeb691766
Armaan253-code <am5432107@gmail.com>
2025-01-31 11:32:12 +0530

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,11 @@
+# Write your code below this line task 1
+print("Hello world!\nHello world\nHello world!")
+print("hello"+" Armaan")
+print("welcome"+" "+"MLN College")
+#how take input from run time
+input("What is your name?")
+#This line of code will take an input using the input() function
+print("Hello "+ input("What is your name?")+ "!")
+# print(len(input("Tell me your goal?"))
+username=input("What is your freind name?")
+print(len(username))
